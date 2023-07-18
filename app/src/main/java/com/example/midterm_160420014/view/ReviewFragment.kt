c467774372db28dd9b3f1125b1c230a9cc6b1359
Fahrizal2408 <fahrizalwijanarko2408@gmail.com>
2023-07-18 23:38:33 +0700

    Comment Review Fixed

--- app/src/main/java/com/example/midterm_160420014/view/ReviewFragment.kt
+++ app/src/main/java/com/example/midterm_160420014/view/ReviewFragment.kt
@@ -1,12 +1,16 @@
 package com.example.midterm_160420014.view
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,6 +19,8 @@ import com.example.midterm_160420014.R
 import com.example.midterm_160420014.viewModel.ListReviewViewModel
 import com.example.midterm_160420014.viewModel.RestoDetailViewModel
 import com.example.midterm_160420014.viewModel.UserViewModel
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class ReviewFragment : Fragment() {
     private lateinit var reviewVM: ListReviewViewModel
@@ -24,6 +30,7 @@ class ReviewFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val sharedPref = requireActivity().getSharedPreferences("UserLogin", Context.MODE_PRIVATE)
+        var userid=sharedPref.getString("uuid","")
         val ids = ReviewFragmentArgs.fromBundle(requireArguments())
         reviewVM = ViewModelProvider(this)[ListReviewViewModel::class.java]
         restoVM = ViewModelProvider(this)[RestoDetailViewModel::class.java]
@@ -34,6 +41,15 @@ class ReviewFragment : Fragment() {
         recView.layoutManager= LinearLayoutManager(context)
         recView.adapter=reviewAdapter
         observe()
+        val btnAddReview = view.findViewById<Button>(R.id.btnAddReview)
+        btnAddReview.setOnClickListener{
+            val txtComment = view.findViewById<EditText>(R.id.txtComment).text.toString()
+            reviewVM.addReview(userid!!.toInt(),ids.menuId,ids.restoId,txtComment)
+            Toast.makeText(context,"Comment added", Toast.LENGTH_SHORT).show()
+            reviewVM.viewReview(ids.restoId,ids.menuId)
+            userVM.addAllUserData()
+            observe()
+        }
     }
     fun observe(){
         Log.d("MASUK: ","Masuk")
